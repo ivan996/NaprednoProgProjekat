de94faeafde478c4859857be49cd89e10fee9f49
ivan996 <ivan.stanimirovic692@gmail.com>
2020-06-15 23:06:09 +0200

    Kreiran glavni GUI

--- projekat/src/main/java/rs/ac/bg/fon/ai/projekat/GUI/GlavniGUI.java
+++ projekat/src/main/java/rs/ac/bg/fon/ai/projekat/GUI/GlavniGUI.java
@@ -6,6 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.Rectangle;
+import java.awt.Insets;
 
 public class GlavniGUI extends JFrame {
 
@@ -32,11 +42,54 @@ public class GlavniGUI extends JFrame {
 	 */
 	public GlavniGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 794, 557);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		panel.setMinimumSize(new Dimension(10, 30));
+		panel.setPreferredSize(new Dimension(100, 50));
+		contentPane.add(panel, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("Izaberite Grad:");
+		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblNewLabel.setPreferredSize(new Dimension(100, 30));
+		panel.add(lblNewLabel);
+		
+		JComboBox cmbGrad = new JComboBox();
+		cmbGrad.setPreferredSize(new Dimension(100, 20));
+		panel.add(cmbGrad);
+		
+		JButton btnUcitaj = new JButton("Ucitaj");
+		btnUcitaj.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnUcitaj.setHorizontalTextPosition(SwingConstants.CENTER);
+		panel.add(btnUcitaj);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(null);
+		
+		JPanel trenutnaTemp = new JPanel();
+		trenutnaTemp.setBounds(10, 11, 470, 232);
+		panel_1.add(trenutnaTemp);
+		
+		JPanel slikaTrenutna = new JPanel();
+		slikaTrenutna.setBounds(514, 11, 244, 156);
+		panel_1.add(slikaTrenutna);
+		slikaTrenutna.setLayout(null);
+		
+		JLabel lblSlika = new JLabel("New label");
+		lblSlika.setBounds(0, 0, 244, 156);
+		slikaTrenutna.add(lblSlika);
+		
+		JPanel narednihDana = new JPanel();
+		narednihDana.setBounds(10, 264, 748, 183);
+		panel_1.add(narednihDana);
 	}
-
 }
